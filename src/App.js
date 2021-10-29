aa75527695e81ab5189412ec1a5db626f5e949eb
juancsr <juancsr@protonmail.com>
2021-10-28 19:32:19 -0500

    very soon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          👨‍💻 Under construction
+          👨‍💻 Under construction, very soon
         </p>
         <a
           className="App-link"
